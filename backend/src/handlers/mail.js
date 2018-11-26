fbb4db7d65b0ea8e1aac7e2198c7e110a24bfdb4
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2018-11-26 21:25:14 +0300

    Test mail

--- backend/src/handlers/mail.js
+++ backend/src/handlers/mail.js
@@ -1,8 +1,8 @@
 import nodemailer from 'nodemailer';
 import sparkPostTransport from 'nodemailer-sparkpost-transport';
-import pug from 'pug';
-import juice from 'juice';
-import htmlToText from 'html-to-text';
+// import pug from 'pug';
+// import juice from 'juice';
+// import htmlToText from 'html-to-text';
 
 require('dotenv').config({ path: 'variables.env' });
 
@@ -18,19 +18,34 @@ export const transport = nodemailer.createTransport(
   }),
 );
 
-const generateHTML = (filename, options = {}) =>
-  juice(pug.renderFile(`${__dirname}/../views/${filename}.pug`, options));
+// const generateHTML = (filename, options = {}) =>
+// juice(pug.renderFile(`${__dirname}/../views/${filename}.pug`, options));
 
-export function send(options) {
-  const html = generateHTML(options.filename, options);
-  const text = htmlToText.fromString(html);
-  const mailOptions = {
-    from: options.from,
-    to: options.user.email,
-    subject: options.subject,
-    text,
-    html,
-  };
-
-  return transport.sendMail(mailOptions);
+export function send() {
+  // const html = generateHTML(options.filename, options);
+  // const text = htmlToText.fromString(html);
+  // const mailOptions = {
+  //   from: options.from,
+  //   to: options.user.email,
+  //   subject: options.subject,
+  //   text,
+  //   html,
+  // };
+  transport.sendMail(
+    {
+      from: `FrontView <something@sparkpostbox.com>`,
+      to: 'm3847710@nwytg.net',
+      subject: 'Very important stuff',
+      text: 'Plain text',
+      html: 'Rich taggery',
+    },
+    (err, info) => {
+      if (err) {
+        console.log(`Error: ${err}`);
+      } else {
+        console.log(`Success: ${info}`);
+      }
+    },
+  );
+  // return transport.sendMail(mailOptions);
 }
