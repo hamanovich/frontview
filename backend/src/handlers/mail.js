76f8cfc6df45d2ebfed16175f89ad13886674ade
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2018-11-26 22:17:33 +0300

    sendgrid

--- backend/src/handlers/mail.js
+++ backend/src/handlers/mail.js
@@ -1,61 +1,39 @@
 import nodemailer from 'nodemailer';
-import mg from 'nodemailer-mailgun-transport';
-// import sparkPostTransport from 'nodemailer-sparkpost-transport';
-// import pug from 'pug';
-// import juice from 'juice';
-// import htmlToText from 'html-to-text';
+import sgTransport from 'nodemailer-sendgrid-transport';
+import pug from 'pug';
+import juice from 'juice';
+import htmlToText from 'html-to-text';
 
 require('dotenv').config({ path: 'variables.env' });
 
-const auth = {
+const options = {
   auth: {
-    api_key: '58012163db87745d51fc6a0185f0ac03-059e099e-e8e02ecb',
-    domain: 'sandbox23e4ab9def8c433da9c06792ebc41e7d.mailgun.org',
+    api_user: 'app113815107@heroku.com',
+    api_key: 'wdom3wha6547',
   },
 };
 
-export const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+export const transport = nodemailer.createTransport(sgTransport(options));
 
-// export const transport = nodemailer.createTransport(
-//   sparkPostTransport({
-//     sparkPostApiKey: process.env.SPARKPOST_API_KEY,
-//     host: process.env.SPARKPOST_SMTP_HOST,
-//     port: process.env.SPARKPOST_SMTP_PORT,
-//     auth: {
-//       user: process.env.SPARKPOST_SMTP_USERNAME,
-//       pass: process.env.SPARKPOST_SMTP_PASSWORD,
-//     },
-//   }),
-// );
+const generateHTML = (filename, options = {}) =>
+  juice(pug.renderFile(`${__dirname}/../views/${filename}.pug`, options));
 
-// const generateHTML = (filename, options = {}) =>
-// juice(pug.renderFile(`${__dirname}/../views/${filename}.pug`, options));
-
-export function send() {
-  // const html = generateHTML(options.filename, options);
-  // const text = htmlToText.fromString(html);
-  // const mailOptions = {
-  //   from: options.from,
-  //   to: options.user.email,
-  //   subject: options.subject,
-  //   text,
-  //   html,
-  // };
-  nodemailerMailgun.sendMail(
-    {
-      from: 'postmaster@sandbox23e4ab9def8c433da9c06792ebc41e7d.mailgun.org',
-      to: 'm3847710@nwytg.net',
-      subject: 'Hey you, awesome!',
-      html: '<b>Wow Big powerful letters</b>',
-      text: 'Mailgun rocks, pow pow!',
-    },
-    (err, info) => {
-      if (err) {
-        console.log(`Error: ${err}`);
-      } else {
-        console.log(`Response: ${info}`);
-      }
-    },
-  );
+export function send(options) {
+  const html = generateHTML(options.filename, options);
+  const text = htmlToText.fromString(html);
+  const mailOptions = {
+    from: options.from,
+    to: options.user.email,
+    subject: options.subject,
+    text,
+    html,
+  };
+  transport.sendMail(mailOptions, (err, res) => {
+    if (err) {
+      console.log('================', err);
+    }
+    console.log('RRRRRRRRRRRR', res);
+    console.log('EMAIL', options);
+  });
   // return transport.sendMail(mailOptions);
 }
