516adc7ec07b31fbc37c332f7300e80d33662e6b
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2018-11-26 20:28:24 +0300

    Install sparkpost

--- backend/src/handlers/mail.js
+++ backend/src/handlers/mail.js
@@ -1,21 +1,22 @@
 import nodemailer from 'nodemailer';
+import sparkPostTransport from 'nodemailer-sparkpost-transport';
 import pug from 'pug';
 import juice from 'juice';
 import htmlToText from 'html-to-text';
 
 require('dotenv').config({ path: 'variables.env' });
 
-export const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+export const transport = nodemailer.createTransport(
+  sparkPostTransport({
+    sparkPostApiKey: process.env.SPARKPOST_API_KEY,
+    host: process.env.SPARKPOST_SMTP_HOST,
+    port: process.env.SPARKPOST_SMTP_PORT,
+    auth: {
+      user: process.env.SPARKPOST_SMTP_USERNAME,
+      pass: process.env.SPARKPOST_SMTP_PASSWORD,
+    },
+  }),
+);
 
 const generateHTML = (filename, options = {}) =>
   juice(pug.renderFile(`${__dirname}/../views/${filename}.pug`, options));
@@ -30,6 +31,19 @@ export function send(options) {
     text,
     html,
   };
+
+  // transport.sendMail({
+  //   from: 'test@your-sending-domain.com',
+  //   to: 'someone@somedomain.com',
+  //   subject: 'Hello from nodemailer-sparkpost-transport',
+  //   html: '<p>Hello world</p>'
+  // }, (err, info) => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     console.log(info);
+  //   }
+  // })
 
   return transport.sendMail(mailOptions);
 }
